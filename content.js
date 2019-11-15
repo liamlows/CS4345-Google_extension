fa586000bbc547f7eacc9c1812d5a5f1c0799541
Harrison Noble <harrisonnoble@yahoo.com>
2019-11-15 17:52:10 -0600

    added functionality to add a word list

--- content.js
+++ content.js
@@ -1,35 +1,19 @@
 blurToggle = (listOWords) => {
 
-  console.log("function called.\n")
+  console.log("function called.\n");
   var matches = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, span");
+
   if (matches.length > 0){
-    matches.forEach(match => {
-      // console.log(match.innerText.includes('simple') + ", " + match.innerText);
-      if(match.innerText.includes('of')){
-        // console.log("one contains\n");
-        match.style.filter = 'blur(0.2rem)';
-      }
+    listOWords.forEach(word => {
+      matches.forEach(match => {
+        // console.log(match.innerText.includes('simple') + ", " + match.innerText);
+        if(match.innerText.toUpperCase().includes(word.toUpperCase())){
+          // console.log("one contains\n");
+          match.style.filter = 'blur(0.2rem)';
+        }
+      });
     });
   }
 }
 
-
-
-//   var elem = $( "p:contains('whats')" );
-//   if(elem.length > 0){
-//     console.log(elem);
-//     console.log("P tag found.\n")
-//     if(elem.hasClass("blur")){
-//       elem.removeClass("blur");
-//     }
-//     else{
-//       // elem.addClass("blur");
-//       elem.css("filter", "blur(0.2rem)");
-//     }
-//   }else{
-//     console.log("No p tags found with whats.\n")
-//     console.log(elem);
-//   }
-// }
-
-blurToggle();
+blurToggle(['Game of thrones', 'Greys anatomy', 'javascript', 'poopoo']);
