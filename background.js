1831cc55ee07279616eda2576a5df76ef0b14b23
liamlows <liam.lowsleywilliams@gmail.com>
2019-11-15 15:23:20 -0600

    changeup

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+    chrome.storage.sync.set({hide: true}, function() {
+      console.log("Hide image is on");
+    });
   });
+
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
+        pageUrl: {hostEquals: 'www.quora.com'},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
-});
+
