d2e5c3ef92e84a19b35b7beec2a7d86f8f3ff3cb
liamlows <liam.lowsleywilliams@gmail.com>
2019-11-15 18:41:43 -0600

    array support - broken form submit

--- content.js
+++ content.js
@@ -2,18 +2,19 @@ blurToggle = (listOWords) => {
 
   console.log("function called.\n");
   var matches = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, span");
-
-  if (matches.length > 0){
-    listOWords.forEach(word => {
-      matches.forEach(match => {
-        // console.log(match.innerText.includes('simple') + ", " + match.innerText);
-        if(match.innerText.toUpperCase().includes(word.toUpperCase())){
-          // console.log("one contains\n");
-          match.style.filter = 'blur(0.2rem)';
-        }
-      });
-    });
-  }
+  console.log(listOWords);
+  
+  // if (matches.length > 0){
+  //   listOWords.forEach(word => {
+  //     matches.forEach(match => {
+  //       // console.log(match.innerText.includes('simple') + ", " + match.innerText);
+  //       if(match.innerText.toUpperCase().includes(word.toUpperCase())){
+  //         // console.log("one contains\n");
+  //         match.style.filter = 'blur(0.2rem)';
+  //       }
+  //     });
+  //   });
+  // }
 }
 
-blurToggle(['Game of thrones', 'Greys anatomy', 'javascript', 'poopoo']);
+// blurToggle(['Game of thrones', 'Greys anatomy', 'otherwise', 'example']);
