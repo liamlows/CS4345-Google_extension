3c1e8cd14e57c5d0321ff78579cce7d6ac168b5e
Harrison Noble <harrisonnoble@yahoo.com>
2019-11-18 19:41:41 -0600

    no errors baby

--- PURE_JS_Extension/content.js
+++ PURE_JS_Extension/content.js
@@ -1,15 +1,15 @@
+var list = JSON.parse(localStorage.getItem('wordList'));
+
 blurToggle = (listOWords) => {
 
   console.log("function called.\n");
-  var matches = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, span");
   console.log(listOWords);
+  var matches = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, span");
   
   if (matches.length > 0){
     listOWords.forEach(word => {
       matches.forEach(match => {
-        // console.log(match.innerText.includes('simple') + ", " + match.innerText);
         if(match.innerText.toUpperCase().includes(word.toUpperCase())){
-          // console.log("one contains\n");
           match.style.filter = 'blur(0.2rem)';
           match.className = 'blur';
         }
@@ -18,7 +18,7 @@ blurToggle = (listOWords) => {
   }
 }
 
-blurToggle(['since'] /*|| localStorage.getItem('wordList')*/);
+blurToggle(list);
 
 
 
--- PURE_JS_Extension/popup.js
+++ PURE_JS_Extension/popup.js
@@ -1,5 +1,22 @@
 $(function() {
-    $('#btn').click(function() {
-        alert($('#show-name').val())
+    $('#btn').click(async function() {
+        //create URL
+        let val = $('#show-name').val();
+        var url = `http://ec2-18-191-182-243.us-east-2.compute.amazonaws.com/show=${val}`;
+         
+        //Call api endpoint
+        await fetch(url, { method:'GET', mode:'cors', credentials:'same-origin' })
+            .then(r => { return r.json() })
+            .then(r => {
+                console.log(r);
+                localStorage.setItem("wordList", JSON.stringify(r));
+            })
+            .catch(r => {
+                alert(r);
+            });
+        
+        //GET LIST OF DATA
+        //PUT IT IN LOCALSTORAGE
+
     });
 });
