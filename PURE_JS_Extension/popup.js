2f91de42487b56220d02f031e7c8678e654a656c
liamlows <liam.lowsleywilliams@gmail.com>
2019-11-20 21:48:02 -0600

    UI changes

--- PURE_JS_Extension/popup.js
+++ PURE_JS_Extension/popup.js
@@ -2,8 +2,10 @@ $(function() {
     $('#btn').click(async function() {
         //create URL
         let val = $('#show-name').val();
-        var url = `ec2-3-17-5-181.us-east-2.compute.amazonaws.com=${val}`;
-         
+        var reg = /[\w]/gi;
+        val = val.match(reg);
+        var url = `http://ec2-18-221-46-124.us-east-2.compute.amazonaws.com=${val}`;
+        document.getElementById("load").classList.add('loader');
         //Call api endpoint
         await fetch(url, { method:'GET', mode:'cors', credentials:'same-origin' })
             .then(r => { return r.json() })
@@ -11,9 +13,12 @@ $(function() {
                 console.log(r);
                 //Store list of words
                 chrome.storage.sync.set({ wordList: r }, () => {});
+                document.getElementById("load").classList.remove('loader');
+
             })
             .catch(r => {
                 alert(r);
             });
     });
+
 });
