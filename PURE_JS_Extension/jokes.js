c16b333abe6d0c9db2b327321fda3474aff9ded5
liamlows <liam.lowsleywilliams@gmail.com>
2019-11-20 23:11:51 -0600

    jokes and cleanup

---
+++ PURE_JS_Extension/jokes.js
@@ -0,0 +1,15 @@
+
+window.onload = () => {
+    async function joke() {
+        await fetch('https://icanhazdadjoke.com/', { method:'GET', mode:'cors', credentials:'same-origin' , headers: {Accept: 'application/json'} })
+            .then(r => {      
+                return r.json() })
+            .then(r => {
+                document.getElementById("joke").innerHTML=r.joke;
+            })
+            .catch(r => {
+                document.getElementById("joke").innerHTML="Joke Connection Error";
+            });
+    }
+    joke();
+}
--- PURE_JS_Extension/popup.js
+++ PURE_JS_Extension/popup.js
@@ -2,10 +2,14 @@ $(function() {
     $('#btn').click(async function() {
         //create URL
         let val = $('#show-name').val();
-        var reg = /[\w]/gi;
-        val = val.match(reg);
-        var url = `http://ec2-18-221-46-124.us-east-2.compute.amazonaws.com=${val}`;
+        var reg = /[\w\s]+/gi;
+        val = val.match(reg).join();
+        console.log(val);
+
         document.getElementById("load").classList.add('loader');
+        var url = `http://ec2-18-221-46-124.us-east-2.compute.amazonaws.com/show=${val}`;
+        console.log(url);
+
         //Call api endpoint
         await fetch(url, { method:'GET', mode:'cors', credentials:'same-origin' })
             .then(r => { return r.json() })
